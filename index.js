a42c166ddc44d2afe98887e1490fa13d9fa0ec5d
Matt Travi <programmer@travi.org>
2019-05-30 22:15:55 -0500

    feat(storybook): defined rules for files in the storybook directory

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: '@form8ion/react'
+  extends: '@form8ion/react',
+
+  overrides: {
+    files: '.storybook/*.js',
+    rules: {
+      'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
+    }
+  }
 };
